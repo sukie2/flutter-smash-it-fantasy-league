e64073a47c833a6d9d36ef4537433199af02acc4
Sukie2 <sukie2@gmail.com>
2021-07-20 10:58:54 +0800

    UI Refactoring

--- lib/widgets/player_row.dart
+++ lib/widgets/player_row.dart
@@ -9,10 +9,31 @@ class PlayerRow extends StatelessWidget {
       padding: EdgeInsets.only(left: 16.0, top: 2.0, bottom: 2.0, right: 16.0),
       child: Card(
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             CircleAvatar(
-              child: Text('SU'),
-            )
+              child: Text('DS'),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Dasun Shanaka'),
+                Text('Batting allrounder'),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('\$ 4,000'),
+                Icon(
+                  Icons.check,
+                  color: Colors.greenAccent,
+                  size: 20.0,
+                ),
+              ],
+            ),
           ],
         ),
       ),
