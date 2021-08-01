cffe512a8d9c7c8017f3695f8b3a797e9fa3ad9b
Sukie2 <sukie2@gmail.com>
2021-08-01 10:29:27 +0800

    Basic home UI added

--- lib/constants/string_constants.dart
+++ lib/constants/string_constants.dart
@@ -3,4 +3,5 @@ class StringConstants {
   static const error_user_registration = 'Error occured while registering';
   static const error_login_fail = 'Username and password don\'t match';
   static const success_user_registration = 'You have been signed up';
+  static const upcoming_matches = 'Upcoming matches';
 }
--- lib/ui/game/home_screen.dart
+++ lib/ui/game/home_screen.dart
@@ -1,24 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:smash_it/controllers/auth_controller.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Cric Fantasy League Home"),
-            TextButton(
-              onPressed: () {
-                AuthController.to.signOut();
-              },
-              child: const Text('Sign out'),
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.mail),
+            title: new Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), title: Text('Profile'))
+        ],
       ),
+      body: Container(
+          child: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            // Display a placeholder widget to visualize the shrinking size.
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Score'),
+            ),
+            expandedHeight: 100,
+            collapsedHeight: 60,
+          ),
+          SliverAppBar(
+            // Display a placeholder widget to visualize the shrinking size.
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Upcoming Matches'),
+            ),
+            expandedHeight: 100,
+            collapsedHeight: 60,
+            pinned: true,
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  color: Colors.blueGrey,
+                  height: 40,
+                  child: Text('Match $index'),
+                );
+              },
+              childCount: 100,
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
