b7b8a87d0ee5de00f0d372e9b46d8b0e885ed8db
Sukitha <sudugamasooriya@palo-it.com>
2021-08-25 17:13:56 +0800

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:smash_it/controllers/home_controller.dart';
 import 'package:smash_it/controllers/profile_controller.dart';
 
 import 'controllers/auth_controller.dart';
-import 'ui/smash_it_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,5 +12,5 @@ void main() async {
   Get.put<AuthController>(AuthController());
   Get.put<HomeController>(HomeController());
   Get.put<ProfileController>(ProfileController());
-  runApp(SmashItApp());
+  // runApp(SmashItApp());
 }
