bc7e83a3fac19bf96bf024f5feb18be41db03165
sukie2 <sukitha.dev@gmail.com>
2021-08-28 18:55:57 +0800

    Work on player selection task- partial

--- lib/ui/palyer_selection_screen.dart
+++ lib/ui/palyer_selection_screen.dart
@@ -11,37 +11,59 @@ class PlayerSelectionScreen extends StatefulWidget {
 class _PlayerSelectionScreenState extends State<PlayerSelectionScreen> {
   List<PlayerRowData> playerList = [
     PlayerRowData(Player('Wanindu Hasaranga', 9000, 'Bowling allrounder')),
+    PlayerRowData(Player('Dasun Shanaka', 5000, 'Batting allrounder')),
+    PlayerRowData(Player('Dasun Shanaka1', 5000, 'Batting allrounder')),
+    PlayerRowData(Player('Dasun Shanaka', 5000, 'Batting allrounder')),
+    PlayerRowData(Player('Dasun Shanaka', 5000, 'Batting allrounder')),
     PlayerRowData(Player('Dasun Shanaka', 5000, 'Batting allrounder'))
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            title: Text('Sliver App Bar'),
-            expandedHeight: 125.0,
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return PlayerRow(playerList[index]);
-              },
-              childCount: 2,
+        body: Column(
+      children: <Widget>[
+        Expanded(
+          flex: 3,
+          child: Container(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  title: Text('Sliver App Bar'),
+                  expandedHeight: 100.0,
+                ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return PlayerRow(playerList[index]);
+                    },
+                    childCount: playerList.length,
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: RaisedButton(
-          onPressed: () {},
-          color: Colors.blue,
-          textColor: Colors.white,
-          child: Text('Fixed Button'),
         ),
-      ),
-    );
+        Expanded(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.all(0),
+            width: double.infinity,
+            child: GridView.count(
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 6,
+              children: List.generate(12, (index) {
+                return CircleAvatar(
+                  child: Text(
+                    'P $index',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                );
+              }),
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
