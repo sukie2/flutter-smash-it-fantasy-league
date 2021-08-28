f91b020b2b0bfd2e3c9801c196be564a7bcabbe2
sukie2 <sukitha.dev@gmail.com>
2021-08-28 14:59:37 +0800

    Work on player selection task- partial

--- lib/ui/palyer_selection_screen.dart
+++ lib/ui/palyer_selection_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smash_it/models/player.dart';
 import 'package:smash_it/models/player_row_data.dart';
-import 'package:smash_it/ui/widgets/game_info_widget.dart';
 import 'package:smash_it/ui/widgets/player_row.dart';
 
 class PlayerSelectionScreen extends StatefulWidget {
@@ -19,42 +18,29 @@ class _PlayerSelectionScreenState extends State<PlayerSelectionScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
-        slivers: [
+        slivers: <Widget>[
           SliverAppBar(
-            pinned: true,
-            floating: true,
-            expandedHeight: 200,
-            backgroundColor: Colors.blueGrey,
-            flexibleSpace: FlexibleSpaceBar(
-              background: GameInfoWidget(),
-              title: Text('SL vs IND'),
-            ),
+            title: Text('Sliver App Bar'),
+            expandedHeight: 125.0,
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
                 return PlayerRow(playerList[index]);
               },
-              childCount: 2, // 1000 list items
+              childCount: 2,
             ),
           ),
-          SliverGrid(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              ///no.of items in the horizontal axis
-              crossAxisCount: 6,
-            ),
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.teal[100 * (index % 9)],
-                  child: Text('grid item $index'),
-                );
-              },
-              childCount: 12,
-            ),
-          )
         ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: RaisedButton(
+          onPressed: () {},
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Text('Fixed Button'),
+        ),
       ),
     );
   }
